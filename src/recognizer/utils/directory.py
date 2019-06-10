68c3e57ca6e0caba87bc2df1158de5686c8ab635
joanna-janos <janos.joanna@gmail.com>
2019-06-10 19:37:36 +0200

    Add utils for creating not existing directories

---
+++ src/recognizer/utils/directory.py
@@ -0,0 +1,14 @@
+import pathlib
+
+
+def create_not_existing_directory(directory: str):
+    """Create not existing directory. If directory exists, do nothing.
+    Parameters
+    ----------
+    directory : directory to create
+
+    """
+    p = pathlib.Path(directory)
+    if not p.is_dir():
+        print(f'Creating directory: {directory} as it does not exist')
+        p.mkdir(parents=True, exist_ok=True)
