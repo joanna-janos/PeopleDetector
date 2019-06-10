d57d838a2fa8298466a21642c0ac0c28868cfe2f
joanna-janos <janos.joanna@gmail.com>
2019-06-10 19:36:52 +0200

    Add utils for getting default data

---
+++ src/recognizer/utils/data.py
@@ -0,0 +1,16 @@
+import glob
+from os.path import normpath, basename
+
+
+def get_data_filenames(path_to_data):
+    """Get filenames of default dataset.
+    Parameters
+    ----------
+    directory : directory with data
+
+    Returns
+    -------
+    List[str]
+            Filenames
+    """
+    return [basename(normpath(f)) for f in glob.glob(path_to_data + "*.jpg")]
