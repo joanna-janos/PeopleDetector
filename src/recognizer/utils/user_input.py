cba7926d12b32f9d580cbbfc59f1f6f8be3e7fd6
joanna-janos <janos.joanna@gmail.com>
2019-06-11 16:31:04 +0200

    Change paths to relative

--- src/recognizer/utils/user_input.py
+++ src/recognizer/utils/user_input.py
@@ -8,31 +8,31 @@ def parse_arguments():
     """Parse user provided arguments."""
 
     parser = argparse.ArgumentParser(
-        description="People recognizer. If you want to recognize people on predefined images left all arguments blank."
+        description="People recognizer."
     )
 
     parser.add_argument(
         "--filenames",
         default="",
-        help="Filenames of file on which people will be recognized. Names MUST be separated by ','."
+        help="Filenames of file on which people will be recognized. Names MUST be separated by ',' and without spaces around them."
     )
 
     parser.add_argument(
         "--data_dir",
-        default="/Users/joannajanos/Documents/informatyka_wmii_uj/semestr2/biometria/PeopleRecognizer/data/",
-        help="Absolute path to files given as filenames argument."
+        default="../../data/",
+        help="Relative path from main.py to files given as filenames argument."
     )
 
     parser.add_argument(
         "--results_dir",
-        default="/Users/joannajanos/Documents/informatyka_wmii_uj/semestr2/biometria/PeopleRecognizer/results/",
-        help="Absolute path to place where results will be stored"
+        default="../../results/",
+        help="Relative path from main.py to place where results will be stored"
     )
 
     parser.add_argument(
         "--yolo_config_path",
-        default="/Users/joannajanos/Documents/informatyka_wmii_uj/semestr2/biometria/PeopleRecognizer/src/recognizer/yolo/config/",
-        help="Absolute path to yolo config"
+        default="yolo/config/",
+        help="Relative path from main.py to yolo config"
     )
 
     args = parser.parse_args()
